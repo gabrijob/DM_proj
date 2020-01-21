7b43cb31f521351224771542b2ec09f143941c9e
Gabriel Job Antunes Grabher <Gabriel.Job-Antunes-Grabher@ensimag.grenoble-inp.fr>
2020-01-22 00:43:27 +0100

    minuit commit

--- machine_removal_analysis.py
+++ machine_removal_analysis.py
@@ -21,42 +21,42 @@ entries.cache()
 
 ##### Get the percentage of cpu loss for maintenance
 
-# First get a RDD with the amount of cpu removed during the processing (id, (event, cpu, 1))
-cpu_removed = entries.filter(lambda x: x[2] != u'2' and x[4] is not u'')\
-	.map(lambda x:(x[1],(int(x[2]),float(x[4]),1)) if x[2]==u'1' else (x[1],(int(x[2]),0,1)))\
-	.reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2]))\
-    .filter(lambda x: x[1][0] != 0)
+### First get a RDD with the amount of cpu removed during the processing (id, (event, cpu, 1))
+### After reducing get pair (id, (nb of removes, cpu, nb removes + updates))
+cpu_rm_ratio = entries.filter(lambda x: x[2] != u'2' and x[4] is not u'')\
+	.map(lambda x:(x[1],(1,float(x[4]),0)) if x[2]==u'1' else (x[1],(0,float(x[4]),1)))\
+	.reduceByKey(lambda x,y: (x[0]+y[0], x[1], x[2]+y[2]))
 
-# Display the behavior of 10 machines where (number of removes, cpu lost, tot number of events)
-"""print("Display the behavior of 10 machines where (number of removes, cpu lost, tot number of events):")
-for elem in cpu_removed.take(10):
-	print(elem)
-"""
 # Get the amount of cpu lost for all removals represented in the form (nb of removals, cpu loss)
-cpu_loss_all = cpu_removed.map(lambda x: (x[1][0], x[1][1])).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
+cpu_loss_all = cpu_rm_ratio.map(lambda x: (x[1][0], x[1][0]*x[1][1])).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
+# Get the amount of cpu added for all additions represented in the form (nb of additions, cpu added)
+cpu_gain_all = cpu_rm_ratio.map(lambda x: (x[1][2], x[1][2]*x[1][1])).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
 
 # Then get the total number of maintenances and the amount of cpu lost by them
-cpu_loss_maintenance = cpu_removed.map(lambda x: (x[1][0], x[1][1]) if 2*x[1][0]+1 == x[1][2] else (x[1][0], x[1][1] - x[1][1]/x[1][0]))\
+cpu_loss_maintenance = cpu_rm_ratio.map(lambda x: (x[1][0], x[1][0]*x[1][1]) if x[1][0]+1 == x[1][2] else (x[1][0] -1, (x[1][0] -1)*x[1][1]))\
     .filter(lambda x: x[0] > 0).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
 
 # From that we can get the percentages
-tot_cpu_loss_all = cpu_loss_all[1] / cpu_loss_all[0] * 100
+tot_cpu_loss_all = cpu_loss_all[1] / cpu_gain_all[1] * 100
 print("The percentage of cpu loss for all removals is {} ".format(tot_cpu_loss_all))
 
-tot_cpu_loss_mtnc = cpu_loss_maintenance[1] / cpu_loss_maintenance[0] * 100
+tot_cpu_loss_mtnc = cpu_loss_maintenance[1] / cpu_gain_all[1] * 100
 print("The percentage of cpu loss for maintenance is {} ".format(tot_cpu_loss_mtnc))
 
 tot_cpu_loss_fail = tot_cpu_loss_all - tot_cpu_loss_mtnc
 print("The percentage of cpu loss for failures is {} ".format(tot_cpu_loss_fail))
 
 ##### Get the percentage of memory loss for maintenance
-mem_removed = entries.filter(lambda x: x[2] != u'2' and x[5] is not u'')\
-	.map(lambda x:(x[1],(int(x[2]),float(x[5]),1)) if x[2]==u'1' else (x[1],(int(x[2]),0,1)))\
-	.reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2]))\
-    .filter(lambda x: x[1][0] != 0)
+mem_rm_ratio = entries.filter(lambda x: x[2] != u'2' and x[5] is not u'')\
+	.map(lambda x:(x[1],(1,float(x[5]),0)) if x[2]==u'1' else (x[1],(0,float(x[5]),1)))\
+	.reduceByKey(lambda x,y: (x[0]+y[0], x[1], x[2]+y[2]))
 
-mem_loss_maintenance = mem_removed.map(lambda x: (x[1][0], x[1][1]) if 2*x[1][0]+1 == x[1][2] else (x[1][0], x[1][1] - x[1][1]/x[1][0]))\
+# Get the amount of cpu added for all additions represented in the form (nb of additions, mem added)
+mem_gain_all = mem_rm_ratio.map(lambda x: (x[1][2], x[1][2]*x[1][1])).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
+
+# Then get the total number of maintenances and the amount of mem lost by them
+mem_loss_maintenance = mem_rm_ratio.map(lambda x: (x[1][0], x[1][0]*x[1][1]) if x[1][0]+1 == x[1][2] else (x[1][0] - 1, (x[1][0] - 1)*x[1][1]))\
     .filter(lambda x: x[0] > 0).reduce(lambda x,y: (x[0] + y[0], x[1] + y[1]))
 
-tot_mem_loss_mtnc = mem_loss_maintenance[1] / mem_loss_maintenance[0] * 100
+tot_mem_loss_mtnc = mem_loss_maintenance[1] / mem_gain_all[1] * 100
 print("The percentage of memory loss for maintenance is {} ".format(tot_mem_loss_mtnc))
